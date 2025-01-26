81dca7e4d0780e6ea308efab1cb2167d51c4092b
grassedprogrammer <the2ndalienboy802@gmail.com>
2025-01-26 14:58:10 +0800

    Add files via upload

--- termine/Main.py
+++ termine/Main.py
@@ -1,2 +1,17 @@
 import platform
+import sys
+import shelve
+import math
+import random
+import time
+import datetime
+
 OS = platform.system()
+windows = False
+timr_tme:float = 0.0
+if OS.lower == "windows":
+    windows = True
+    import os
+
+if __name__ == "__main__":
+    pass
